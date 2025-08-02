89017f1fe0c92a882fafefa4c8c336ec35ac2c9a
will-the-creator <hoopa05.cascade@icloud.com>
2025-08-02 11:29:35 -0600

    commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module dist_cloud
+
+go 1.23.5
+
+require github.com/fsnotify/fsnotify v1.9.0
+
+require golang.org/x/sys v0.13.0 // indirect
--- watcher.go
+++ watcher.go
@@ -1,31 +1,66 @@
 package main
 
-import {
+import (
     "fmt"
+    "github.com/fsnotify/fsnotify"
     "log"
-    "os"
-}
+    "time"
+)
 
-func start_watch(root string) error {
-    watcher, err := createWatcher()
+func n_watcher() {
+    watcher, err := fsnotify.NewWatcher()
     if err != nil {
-        return err
+        log.Fatal("error:", err)
     }
     defer watcher.Close()
-    
-    if err := addRecursiveDirs(watcher, root); err != nil {
-        return err
-    }
 
-    go handleEvents(watcher)
-    
-    fmt.Println("cloud storage at :", root)
-    select {} // block forever (figure out somethign better)
-}
-func handleEvents(watcher *fsnotify.Watcher) {
-    for {
-        select {
-            case event, ok 
+    done := make(chan bool)
+
+    go func() {
+        for {
+            select {
+            case event, ok := <-watcher.Events:
+                if !ok {
+                    return
+                }
+                fmt.Println("event:", event)
+                if event.Op&fsnotify.Write == fsnotify.Write {
+                    fmt.Println("modified file:", event.Name)
+                }
+            case err, ok := <-watcher.Errors:
+                if !ok {
+                    return
+                }
+                fmt.Println("error:", err)
+            }
         }
+    }()
+
+    err = watcher.Add(".") // watch current directory
+    if err != nil {
+        log.Fatal("error:", err)
     }
+    <-done // block forever
+}
+
+func anonymous() {
+    done := make(chan bool)
+
+    go func() {
+        fmt.Println("running in anonymous goroutine")
+        done <- true
+    }()
+
+    <-done // wait for goroutine to finish
+}
+
+func main() {
+    fmt.Println("Starting anonymous func:")
+    anonymous()
+
+    fmt.Println("\nStarting watcher (press Ctrl+C to stop):")
+    n_watcher()
+
+    // We'll never get here because n_watcher blocks forever
+    time.Sleep(1 * time.Second)
 }
